e2e21c45e09b048a438bf7e789691ea230468f0b
Rajesh sharma <rajesh.rajesh@publicissapient.com>
2023-04-17 22:27:26 +0530

    Initial Commit

---
+++ CoronaStat/CoronaStatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoronaStatApp.swift
+//  CoronaStat
+//
+//  Created by Rajesh Rajesh on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CoronaStatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
